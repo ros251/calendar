797359536bec3f536039d54564aaefedb8be4721
ros251 <ros251@protonmail.com>
2021-10-05 23:12:12 -0400

    9 squares and a date

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import NineCubes from './NineCubes.jsx'
+
+const background_styles = {
+  width: '1200px',
+  height: '742px',
+  margin: '20px auto',
+  backgroundColor:'#090909'
+}
+const leftblock_styles = {
+  display: 'inline-block',
+  width: '600px'
+}
+const rightblock_styles = {
+  display: 'inline-block',
+  width: '600px'
+}
 
 function App() {
   return ( 
     <div id='appwrap'>
-      <h1>APPLICATION</h1>
+      <div style={background_styles}>
+        <div id='leftblock' style={leftblock_styles}>
+
+        </div>
+        <div id='rightblock' style={rightblock_styles}>
+          <h1 style={{marginBottom: '-50px', color: 'white', display: 'inline-block', fontSize: '96px'}}>7</h1>
+          <h1 style={{marginBottom: '-50px', color: 'white', display: 'inline-block', fontSize: '48px'}}>Wednesday</h1>
+          <NineCubes/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/js/components/NineCubes.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+const cubewrap_styles = {
+  width: '500px'
+}
+
+const cube_styles = {
+  backgroundColor: '#272727',
+  width: '140px',
+  height: '140px',
+  margin: '5px',
+  display: 'inline-block',
+  float: 'left'
+}
+const one_two_cube_styles = {  
+  backgroundColor: '#272727',
+  width: '140px',
+  height: '140px',
+  margin: '55px 5px 5px 5px',
+  display: 'inline-block',
+  float: 'left'
+}
+
+const todays_cube_styles = {
+  backgroundColor: '#272727',
+  width: '190px',
+  height: '190px',
+  margin: '5px',
+  display: 'inline-block',
+  float: 'left'
+}
+
+const cubes = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine'
+]
+
+function NineCubes() {
+  
+  const make9Cubes = (cubes) => {
+    console.log(cubes)
+    console.log(cubes)
+    return (cubes.map((num, index) => {
+      if(index == 0 || index == 1){
+        return <div style={one_two_cube_styles} key={index}>{num}</div>
+      } else if (index == 2){
+        return <div style={todays_cube_styles} key={index}>{num}</div>
+      } else {
+        return <div style={cube_styles} key={index}>{num}</div>
+      }
+      }))
+  }
+  return (
+    <div id='cubewrap' style={cubewrap_styles}>
+      {make9Cubes(cubes)}
+    </div>
+  )
+}
+export default NineCubes
